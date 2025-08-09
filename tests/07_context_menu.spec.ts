40c9d4f4260f8aec5d864a11ea3b67d3b67b4539
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-08-09 23:40:11 +0200

    test 7

--- tests/07_context_menu.spec.ts
+++ tests/07_context_menu.spec.ts
@@ -1,8 +1,22 @@
-import { test, expect, Page, Locator } from './fixtures/baseTest';
+import { test, expect} from './fixtures/baseTest';
 
 test.describe('Context Menus', () => {
     test.use({testPath: 'context_menu'});
 
+    test('Right click on the box', async ({ page }) => {
+
+        let alertText: string = '';
+
+        page.on('dialog', async dialog => {
+            expect(dialog.type()).toBe('alert');
+            alertText = dialog.message();
+            await dialog.accept();
+            return alertText;
+        });
+
+        await page.locator('#hot-spot').click({button: 'right'});
+        expect(alertText).toBe('You selected a context menu');
+    })
 
 
 });
