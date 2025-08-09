a142d7656a37b592dc69860db9426084475122b4
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-08-09 05:29:01 +0200

    test 4

---
+++ tests/04_broken_images.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from './fixtures/baseTest';
+
+test.describe('Broken Images', () => {
+    test.use({
+        testPath: 'broken_images',
+    });
+
+    const testCases: {index: number, expected: boolean}[] = [
+        { index: 1, expected: true},
+        { index: 2, expected: true},
+        { index: 3, expected: false},
+    ]
+
+    testCases.forEach(({index, expected}) => {
+        test(`image ${index} should ${expected ? '' : 'not '}be broken`, async ({ page }) => {
+            const xpath_selector: string = `//div[@id="content"]//img[${index}]`;
+            const is_broken: boolean = await page.evaluate(
+                (selector: string) :boolean => {
+                    const img = document.evaluate(selector, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                    return img ? img.naturalWidth === 0 : true;
+                },
+                xpath_selector
+            )
+            expect(is_broken).toEqual(expected);
+        })
+    })
+});
