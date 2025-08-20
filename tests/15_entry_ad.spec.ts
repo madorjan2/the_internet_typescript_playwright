9d088ea5794bdbecb6a9bcf7ff41470d65e6872a
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-08-21 01:11:48 +0200

    test 15 - I learned to read...

--- tests/15_entry_ad.spec.ts
+++ tests/15_entry_ad.spec.ts
@@ -8,20 +8,23 @@ test.describe('Entry Ad', () => {
 		await expect(page.locator('.modal')).toBeHidden();
 	});
 
+	test('modal does not reappear after refreshing', async ({ page }) => {
+		await page.locator('.modal-footer p').click();
+		await expect(page.locator('.modal')).toBeHidden();
+
+		await page.reload();
+
+		await expect(page.locator('.modal')).toBeHidden();
+	});
+
 	test('modal reappears on clicking "click here"', async ({ page }) => {
 		await page.locator('.modal-footer p').click();
-        await expect(page.locator('.modal')).toBeHidden();
+		await expect(page.locator('.modal')).toBeHidden();
 
-        await Promise.all([
-            page.reload(),
-            page.waitForLoadState('load'),
-            page.waitForLoadState('domcontentloaded'),
-            page.waitForLoadState('networkidle'),
+		await page.getByRole('link', { name: 'click here' }).click();
 
-        ]);
+		await page.reload();
 
-
-        await page.getByRole('link', { name: 'click here' }).click()
-        await expect(page.locator('.modal')).toBeVisible();
+		await expect(page.locator('.modal')).toBeVisible();
 	});
 });
