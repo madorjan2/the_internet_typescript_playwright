25d6637acbdd9d2af5e2d7fd4fed1b89a58be6e0
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-08-21 00:50:30 +0200

    test 15 CI adjustment try 3 and changed upload folder in workflow

--- tests/15_entry_ad.spec.ts
+++ tests/15_entry_ad.spec.ts
@@ -11,8 +11,8 @@ test.describe('Entry Ad', () => {
 	test('modal reappears on clicking "click here"', async ({ page }) => {
 		await page.locator('.modal-footer p').click();
         await expect(page.locator('.modal')).toBeHidden();
-        await page.waitForTimeout(1000);
-		await page.getByRole('link', { name: 'click here' }).click();
-		await expect(page.locator('.modal')).toBeVisible();
+        await page.reload();
+        await page.getByRole('link', { name: 'click here' }).click()
+        await expect(page.locator('.modal')).toBeVisible();
 	});
 });
